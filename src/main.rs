cadbdf09db4bab366e7cbba1dedc6316910a6fee
Scott A. Idler <scott.a.idler@gmail.com>
2024-06-06 14:22:54 -0700

    working program

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use clap::Parser;
+use eyre::Result;
+
+/// Simple program to spell words in Scrabble format
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The words to be spelled in Scrabble format
+    words: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let input = args.words.join(" ");
+
+    let scrabble_sentence = input
+        .chars()
+        .filter(|c| c.is_alphabetic() || c.is_whitespace())
+        .map(|c| if c.is_whitespace() {
+            " ".to_string()
+        } else {
+            format!(":scrabble-{}:", c.to_lowercase())
+        })
+        .collect::<Vec<String>>()
+        .join("");
+
+    println!("{}", scrabble_sentence);
+
+    Ok(())
+}
+
